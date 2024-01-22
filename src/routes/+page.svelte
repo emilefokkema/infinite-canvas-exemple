50b46c3613a05f1600b818e26ff7aee7bda7d8c1
lblenner <louis.blenner@gmail.com>
2024-01-22 15:06:20 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import TestComponent from "./TestComponent.svelte";
+
+  let screenWidth;
+  let screenHeight;
+</script>
+
+<div
+  class="bg-blue-800 w-full h-screen"
+  bind:clientWidth={screenWidth}
+  bind:clientHeight={screenHeight}
+>
+  <TestComponent width={screenWidth} height={screenHeight} />
+</div>
